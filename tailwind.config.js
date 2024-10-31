bd7a3b8c84c718686316dd0f37155bfaf5ae8636
felixdanie21 <felixminato12@gmail.com>
2024-10-31 23:49:28 +0700

    update contact

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+//** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html', 'portfolio.html'], // Include all HTML files that use Tailwind CSS
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
